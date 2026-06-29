3a5c9c2c6e49d33968675844403edfe294cbd968
ritusarswat27 <ritusarswat27@gmail.com>
2026-06-28 23:29:09 -0700

    Time: 1 ms (90.04%), Space: 14.8 MB (21.7%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.cpp
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -11,18 +11,36 @@
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-        vector<int> arr;
-        ListNode* node = head;
-        while(node != NULL) {
-            arr.push_back(node->val);
-            node = node->next;
+        if(head->next == NULL) return head;
+        // vector<int> arr;
+        // ListNode* node = head;
+        // while(node != NULL) {
+        //     arr.push_back(node->val);
+        //     node = node->next;
+        // }
+        // sort(arr.begin() , arr.end());
+        // node = head;
+        // for(int num : arr) {
+        //     node->val = num;
+        //     node = node->next;
+        // }
+        // return head;  
+
+
+        ListNode* temp = new ListNode(INT_MIN);
+        temp->next = head;
+        ListNode* curr = head;
+        while(curr != NULL && curr->next != NULL) {
+            if(curr->next->val >= curr->val) curr = curr->next;
+            else{
+                ListNode* node = curr->next;
+                curr->next = node->next;
+                ListNode* pre = temp;
+                while(pre->next->val < node->val) pre = pre->next;
+                node->next = pre->next;
+                pre->next = node;
+            }   
         }
-        sort(arr.begin() , arr.end());
-        node = head;
-        for(int num : arr) {
-            node->val = num;
-            node = node->next;
-        }
-        return head;    
+        return temp->next;
     }
 };
