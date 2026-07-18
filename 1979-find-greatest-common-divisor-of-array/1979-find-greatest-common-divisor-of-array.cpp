fa700850086845e926146e72d6326b8041c0374f
Ritu Sarswat <ritusarswat27@gmail.com>
2026-07-18 03:09:26 -0700

    Time: 0 ms (100%), Space: 16.3 MB (31.74%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -5,10 +5,11 @@ public:
         int maxVal = *max_element(nums.begin() , nums.end());
         int minVal = *min_element(nums.begin() , nums.end());
 
-        int gcd = 1;
-        for(int i=1 ; i<= maxVal; i++) {
-            if(minVal % i == 0 && maxVal % i == 0) gcd = i;
-        }
-        return gcd;  
+        // int gcd = 1;
+        // for(int i=1 ; i<= maxVal; i++) {
+        //     if(minVal % i == 0 && maxVal % i == 0) gcd = i;
+        // }
+        // return gcd;  
+        return gcd(maxVal , minVal);
     }
 };
