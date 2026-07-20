55ba9536a79637e2da89a26f6487cebc5cab9bb8
Ritu Sarswat <ritusarswat27@gmail.com>
2026-07-19 22:22:00 -0700

    Time: 0 ms (100%), Space: 9.7 MB (28.2%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -11,23 +11,16 @@ public:
         for(int i=0; i<n; i++) {
             if(visited[s[i] - 'a'] == true) continue;
 
-            if(st.empty()) {
-                st.push(s[i]);
-                visited[s[i] - 'a'] = true;
-            }
-            else {
-                while(!st.empty() && s[i] < st.top() ) {
-                    int id = lastIndex[st.top() - 'a'];
-                    if(id > i) {
-                        visited[st.top() - 'a'] = false;
-                        st.pop();
-                    }
-                    else break;
+            while(!st.empty() && s[i] < st.top()) {
+                int id = lastIndex[st.top() - 'a'];
+                if(id > i) {
+                    visited[st.top() - 'a'] = false;
+                    st.pop();
                 }
-                
-                st.push(s[i]);
-                visited[s[i] - 'a'] = true;
-            }
+                else break;
+            }   
+            st.push(s[i]);
+            visited[s[i] - 'a'] = true;
         }
 
         string ans;
