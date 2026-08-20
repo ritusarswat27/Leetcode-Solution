194a6da03f8ed90c6d06307a5d65ba669ec3e1c8
Ritu Sarswat <ritusarswat27@gmail.com>
2026-08-20 21:34:51 +0530

    Time: 128 ms (7.14%), Space: 41 MB (9.82%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -8,8 +8,41 @@ public:
         vector<vector<int>> distanceGrid(n , vector<int>(n , INT_MAX));
         distanceGrid[0][0] = 1;
 
+        // priority_queue< vector<int> , vector<vector<int>> , greater<vector<int>>> pq;
+        // pq.push({1 , 0 , 0});
+
+        // vector<vector<int>> distances = {
+        //     {0,1} , {1,0} , {0,-1} , {-1,0}, 
+        //     {1,1} , {-1,-1} , {1,-1} , {-1,1}
+        // };
+
+        // while(!pq.empty()) {
+        //     auto it = pq.top();
+        //     int dist = it[0];
+        //     int row = it[1];
+        //     int col = it[2];
+        //     pq.pop();
+
+        //     for(int i=0; i<8; i++) {
+        //         int nRow = row + distances[i][0];
+        //         int nCol = col + distances[i][1];
+
+        //         if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < n && grid[nRow][nCol] == 0) {
+        //             if(dist + 1 < distanceGrid[nRow][nCol]) {
+        //                 distanceGrid[nRow][nCol] = dist + 1;
+        //                 pq.push({distanceGrid[nRow][nCol] , nRow , nCol});
+        //             }
+        //         }
+        //     }
+        // }
+        // return(distanceGrid[n-1][n-1] == INT_MAX) ? -1 : distanceGrid[n-1][n-1]; 
+
+
+
+
+
         priority_queue< vector<int> , vector<vector<int>> , greater<vector<int>>> pq;
-        pq.push({1 , 0 , 0});
+        pq.push({0 , 0});
 
         vector<vector<int>> distances = {
             {0,1} , {1,0} , {0,-1} , {-1,0}, 
@@ -18,9 +51,8 @@ public:
 
         while(!pq.empty()) {
             auto it = pq.top();
-            int dist = it[0];
-            int row = it[1];
-            int col = it[2];
+            int row = it[0];
+            int col = it[1];
             pq.pop();
 
             for(int i=0; i<8; i++) {
@@ -28,14 +60,18 @@ public:
                 int nCol = col + distances[i][1];
 
                 if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < n && grid[nRow][nCol] == 0) {
-                    if(dist + 1 < distanceGrid[nRow][nCol]) {
-                        distanceGrid[nRow][nCol] = dist + 1;
-                        pq.push({distanceGrid[nRow][nCol] , nRow , nCol});
+                    if(distanceGrid[row][col] + 1 < distanceGrid[nRow][nCol]) {
+                        distanceGrid[nRow][nCol] = distanceGrid[row][col] + 1;
+                        pq.push({nRow , nCol});
                     }
                 }
             }
         }
-        // if(distanceGrid[n-1][n-1] == INT_MAX) return -1;
-        return(distanceGrid[n-1][n-1] == INT_MAX) ? -1 : distanceGrid[n-1][n-1];    
+        return(distanceGrid[n-1][n-1] == INT_MAX) ? -1 : distanceGrid[n-1][n-1]; 
+
+
+
+
+         
     }
 };
