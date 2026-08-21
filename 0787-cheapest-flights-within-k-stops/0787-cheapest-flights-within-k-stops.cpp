e9db294f5fec93aae311613a3415496382a11e2d
Ritu Sarswat <ritusarswat27@gmail.com>
2026-08-21 07:51:22 +0530

    Time: 1 ms (72.29%), Space: 18.7 MB (25.38%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -9,21 +9,31 @@ public:
         vector<int> distance(n , INT_MAX);
         distance[src] = 0;
 
-        queue<pair<int , pair<int , int>>> q;
-        q.push({0 , {src , 0}});
+        // queue<pair<int , pair<int , int>>> q;
+        // q.push({0 , {src , 0}});
 
-        while(!q.empty()) {
-            int stop = q.front().first;
-            int node = q.front().second.first;
-            int cost = q.front().second.second;
-            q.pop();
+        priority_queue< vector<int> , vector<vector<int>> , greater<vector<int>>> pq;
+        pq.push({0 , src , 0});
+
+        while(!pq.empty()) {
+            // int stop = q.front().first;
+            // int node = q.front().second.first;
+            // int cost = q.front().second.second;
+            // q.pop();
+
+            auto it = pq.top();
+            int stop = it[0];
+            int node = it[1];
+            int cost = it[2];
+            pq.pop();
 
             if(stop > k) continue;
             
             for(auto it : adj[node]) {
                 if(cost + it.second < distance[it.first]) {
                     distance[it.first] = cost + it.second;
-                    q.push({stop + 1 , {it.first , distance[it.first]}});
+                    // q.push({stop + 1 , {it.first , distance[it.first]}});
+                    pq.push({stop + 1 , it.first , distance[it.first]});
                 }
             }   
         }
