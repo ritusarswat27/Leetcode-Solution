eff9e52bd194d314cd9bb9a040c8c0c179d15e0e
Ritu Sarswat <ritusarswat27@gmail.com>
2026-09-03 06:27:40 +0530

    Time: 4 ms (65.18%), Space: 165.9 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -5,40 +5,15 @@ public:
         if(n == 1) return true;
 
         int mini = INT_MAX;
-        int minIndex = -1;
-        for(int i=0; i<n; i++) {
-            if(nums1[i] < mini) {
-                mini = min(mini , nums1[i]);
-                minIndex = i;
-            }
-        }
+        for(int i=0; i<n; i++) mini = min(mini , nums1[i]);
 
         int minOdd = INT_MAX; 
         for (int i = 0; i < n; i++) { 
             if (nums1[i] % 2 != 0) minOdd = min(minOdd, nums1[i]); 
         }  
-        
+
         if (mini % 2 == 0 && minOdd != INT_MAX && minOdd > mini) return false;
 
-        // bool even = false , odd = false;
-        // if(mini % 2 == 0) even = true;
-        // else odd = true;
-
-        // for(int i=0; i<n; i++) {
-        //     if(nums1[i] == mini) {
-        //         nums2[i] = mini;
-        //     }
-        //     else if(even == true) {
-        //         if(nums1[i] % 2 == 0) nums2[i] = nums1[i];
-        //         else if(nums1[i] - nums1[j] >=1 && (nums1[i] - nums1[j]) % 2 == 0) nums2[i] = nums1[i] - nums1[j];
-        //         else return false;
-        //     }
-        //     else if(odd == true) {
-        //         if(nums1[i] % 2 != 0) nums2[i] = nums1[i];
-        //         else if(nums1[i] - nums1[j] >=1 && (nums1[i] - nums1[j]) % 2 != 0) nums2[i] = nums1[i] - nums1[j];
-        //         else return false;
-        //     }
-        // }
         return true;
     }
 };
