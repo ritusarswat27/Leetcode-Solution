1e5227222e4346d6396ae3705ad626022eba61a5
Ritu Sarswat <ritusarswat27@gmail.com>
2026-09-09 14:36:25 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.7%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -3,10 +3,8 @@ public:
     int countCommas(int n) {
         if(n < 1000) return 0;
         int cnt = 0;
-        int num = 0;
-        for(int i=1000; i<= n; i++) {
-            num++;
-        }
-        return num;
+        // for(int i=1000; i<= n; i++) cnt++;
+        cnt = n - 1000 + 1;
+        return cnt;
     }
 };
