46dfc2d7b82465ba08397b5aed9bc6892e7da1e5
Ritu Sarswat <ritusarswat27@gmail.com>
2026-09-10 23:08:22 +0530

    Time: 0 ms (100%), Space: 15.8 MB (64.72%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -26,19 +26,37 @@ int findAverage(TreeNode* root) {
     return sum / cnt;
 }
 
-    int averageOfSubtree(TreeNode* root) {
-        int cnt = 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-            if(node->left != NULL) q.push(node->left);
-            if(node->right != NULL) q.push(node->right); 
-            int ans = findAverage(node);
-            if(ans == node->val) cnt++;
-        }
+pair<int , int> dfs(TreeNode* root , int& ans) {
+    if(root == NULL) return {0 , 0};
 
-        return cnt;    
+    auto left = dfs(root->left , ans);
+    auto right = dfs(root->right , ans);
+
+    int sum = left.first + right.first + root->val;
+    int cnt = left.second + right.second + 1;
+
+    if(sum / cnt == root->val) ans++;
+    return {sum , cnt};
+}
+
+    int averageOfSubtree(TreeNode* root) {
+        // int cnt = 0;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()) {
+        //     TreeNode* node = q.front();
+        //     q.pop();
+        //     if(node->left != NULL) q.push(node->left);
+        //     if(node->right != NULL) q.push(node->right); 
+        //     int ans = findAverage(node);
+        //     if(ans == node->val) cnt++;
+        // }
+
+        // return cnt;  
+
+
+        int ans = 0;
+        dfs(root , ans);
+        return ans;
     }
 };
